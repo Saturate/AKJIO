a7fadd34b44bda120c0b16a4ce8172c8397811d2
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2016-06-19 22:11:30 +0200

    Streams

--- water.js
+++ water.js
@@ -1,29 +1,63 @@
 /*
-	GROW
+	WATER
 
 	Enables you yo take some content and grow it into a site.
+
+	It does not optimize anything, it just makes Markdown into HTML.
+	It uses a special header in each file to get meta-data about the current file.
+
+	For optimizing you could plug Water into a Gulp pipe, that could minify assets and such.
 */
 'use strict';
 
-import fs from 'fs';
-import through from 'through2'; // npm install --save through2
+const fs = require('fs');
+const through = require('through2'); // npm install --save through2
+const matter = require('gray-matter'); // npm i gray-matter --save
+const marked = require('marked'); // npm install marked --save
+const nunjucks = require('nunjucks'); // npm install nunjucks --save
 
 // Consts
-const NAME = 'Water';
 
-function Water () {
+function water () {
 	console.log('water!');
-	this.settings = {
-		template: 'template/'
+
+	var defaults = {
+		template: '/template',
+		contentPath: '/content',
+		pages: '/pages',
+		posts: '/posts'
 	};
+
+	//this.settings = defaults;
 }
 
 var test = function() {
 	return through.obj(function(file, encoding, callback) {
-		callback(null, Water(file));
+		console.log(file);
+		callback(null, water(file));
 	});
 };
-fs.createReadStream('README.md').pipe(test);
+
+fs.readdir('content/pages', function(err, files) {
+	console.log(files);
+	var data = '';
+
+	var readableStream = fs.createReadStream('content/pages/' + files[7]);
+
+	readableStream.on('data', function(chunk) {
+		data+=chunk;
+	});
+
+	readableStream.on('end', function() {
+		var foo = matter(data);
+		var content = marked(foo.content);
+		content = nunjucks.renderString(content, foo.data);
+
+		console.log('CONTENT (%s - %s):\n\n', foo.data.title, files[6], content);
+	});
+});
+
+//fs.createReadStream('README.md').pipe(test);
 
 // Gulp
 module.exports = test;
