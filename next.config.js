d27c2f3962fc063106ae61bd60a64fe7d1c0d96c
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2023-11-22 18:53:43 +0100

    Security headers
    Migrate security headers to nextjs config and middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "X-Frame-Options",
+						value: "SAMEORIGIN",
+					},
+					{
+						key: "Permissions-Policy",
+						value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "origin-when-cross-origin",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
