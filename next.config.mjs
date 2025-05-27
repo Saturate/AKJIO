377ca4f83c7a29a1dc6e3a6f214bd507869388c9
Allan Kimmer Jensen <hi@akj.io>
2025-05-27 07:31:17 +0200

    Use @next/mdx
    I don't need the remote part of mdx, so I'll keep it simple for now.

---
+++ next.config.mjs
@@ -0,0 +1,47 @@
+import createMDX from "@next/mdx";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import rehypePrettyCode from "rehype-pretty-code";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "X-Frame-Options",
+						value: "SAMEORIGIN",
+					},
+					{
+						key: "Permissions-Policy",
+						value:
+							"camera=(), microphone=(), geolocation=(), browsing-topics=()",
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "origin-when-cross-origin",
+					},
+				],
+			},
+		];
+	},
+};
+
+const withMDX = createMDX({
+	// Add markdown plugins here, as desired
+	options: {
+		remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+		rehypePlugins: [[rehypePrettyCode, { theme: "ayu-dark" }]],
+	},
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
