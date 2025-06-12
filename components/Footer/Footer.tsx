0e8c0f8f3cfc80355562df630b33d8c1c6677751
Allan Kimmer Jensen <hi@akj.io>
2025-06-12 09:01:24 +0200

    Add ©

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			Copyright 2025 - Allan Kimmer Jensen
+			Copyright © 2025 - Allan Kimmer Jensen
 		</footer>
 	);
 }
